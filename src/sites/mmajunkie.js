af08532e44535c098d0a9d9fa5179c69b0b29327
Jonathan GUILLOT <jonathan@joggee.fr>
2022-06-03 15:59:28 +0200

    Manage multi-authors for MMA Junkie

--- src/sites/mmajunkie.js
+++ src/sites/mmajunkie.js
@@ -1,15 +1,17 @@
 import Site from '../site.js';
-import Info from '../info.js';
 
 export default
 class MmaJunkie extends Site {
     _parse(soup, info) {
-        let item = soup.find('span', { itemprop: 'author' });
-        if (!item)
-            throw new Error('Unable to find author item');
+        let items = soup.findAll('span', { itemprop: 'author' });
+        if (!items)
+            throw new Error('Unable to find author items');
 
-        if (item.text != 'MMA Junkie Staff') {
-            info.authors.push(item.text);
+        for (const item of items) {
+            if (    (item.text !== 'MMA Junkie Staff')
+                 && (info.authors.indexOf(item.text) < 0)) {
+                info.authors.push(item.text);
+            }
         }
 
         super._parse(soup, info);
