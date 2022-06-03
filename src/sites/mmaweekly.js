da726fc1f22678410362c83c38ade39ff91c3753
Jonathan GUILLOT <jonathan@joggee.fr>
2022-06-03 16:09:43 +0200

    Remove now unneeded custom MMA Weekly title parsing
    Correct title is already retrieved from JSON-LD parsing

--- src/sites/mmaweekly.js
+++ src/sites/mmaweekly.js
@@ -4,11 +4,6 @@ export default
 class MmaWeekly extends Site {
     _parse(soup, info) {
         info.site = 'MMA Weekly'
-
         super._parse(soup, info);
-
-        let titleRegex = new RegExp(/(.*) \| MMAWeekly\.com/);
-        let match = info.title.match(titleRegex);
-        info.title = match ? match[1] : info.title;
     }
 }
