e7f31aa672944d7286f0f09ce8cdc81fc28d6bb1
Jonathan GUILLOT <jonathan@joggee.fr>
2023-03-08 12:54:52 +0100

    Fix authors retrieval for MMA Junkie

--- src/sites/mmajunkie.js
+++ src/sites/mmajunkie.js
@@ -3,9 +3,14 @@ import Site from '../site.js';
 export default
 class MmaJunkie extends Site {
     _parse(soup, info) {
-        let items = soup.findAll('span', { itemprop: 'author' });
+        // Get first div only as the page may display several articles
+        let div = soup.find('div', { itemprop: 'author' });
+        if (!div)
+            throw new Error('Unable to find authors div');
+
+        let items = div.findAll('a', { itemprop: 'name' });
         if (!items)
-            throw new Error('Unable to find author items');
+            throw new Error('Unable to find authors item');
 
         for (const item of items) {
             if (    (item.text !== 'MMA Junkie Staff')
